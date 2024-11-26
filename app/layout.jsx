4a24a783b670046d663956b4ca5d5e17156754ea
mamun <mamun.orrashid@brainstation-23.com>
2024-11-26 13:31:23 +0600

    Add home page components

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,5 @@
 import "@/assets/styles/globals.css";
+import Footer from "@/components/Footer";
 import Navbar from "@/components/Navbar";
 export const metadata = {
   title: "PropertyPulse | Find perfect rentals",
@@ -12,7 +13,8 @@ const MainLayout = ({ children }) => {
     <html lang="en">
       <body>
         <Navbar />
-        <div>{children}</div>
+        <main>{children}</main>
+        <Footer/>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -1,10 +1,15 @@
 import Hero from "@/components/Hero";
+import HomeProperties from "@/components/HomeProperties";
 import InfoBoxes from "@/components/InfoBoxes";
-const HomePage = () => {
+import connectDB from "@/config/dababase";
+const HomePage = async () => {
+  console.log(process.env.MONGODB_URI, "ENV------");
+  await connectDB();
   return (
     <>
       <Hero/>
       <InfoBoxes/>
+      <HomeProperties/>
     </>
   );
 };
